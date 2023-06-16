3368e7fbda5d94c34608242b26d7395c6f5306bd
mryao <git@mryao.org>
2023-06-16 23:23:14 +0800

    add aegis support

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -18,9 +18,6 @@ Carefully heed the user's instructions.
 Respond using Markdown.`;
 
 export const modelOptions: ModelOptions[] = [
-  'gpt-3.5-turbo',
-  'gpt-3.5-turbo-16k',
-  'gpt-4',
   // 'gpt-4-32k',
   // 'gpt-3.5-turbo-0301',
   'gpt-3.5-turbo-0613',
@@ -29,6 +26,9 @@ export const modelOptions: ModelOptions[] = [
   'gpt-4-0613',
   // 'gpt-4-32k-0314',
   // 'gpt-4-32k-0613',
+  'gpt-3.5-turbo',
+  'gpt-3.5-turbo-16k',
+  'gpt-4',
 ];
 
 export const defaultModel = 'gpt-3.5-turbo-0613';
